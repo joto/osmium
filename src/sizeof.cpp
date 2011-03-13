6747a07044fe45f0805a21ba43b9064888a5b0f6
Jochen Topf <jochen@topf.org>
2011-03-13 10:10:42 +0100

    add small utility program to check sizes of classes for development

---
+++ src/sizeof.cpp
@@ -0,0 +1,23 @@
+/*
+
+  This is a small tool to find out the sizes of some basic classes.
+  It is only used for Osmium development.
+
+*/
+
+
+#include <stdlib.h>
+#include "osmium.hpp"
+
+int main() {
+    std::cout << "sizeof(Osmium::OSM::Object)="                   << sizeof(Osmium::OSM::Object) << "\n";
+    std::cout << "sizeof(Osmium::OSM::Node)="                     << sizeof(Osmium::OSM::Node)                     << "  (Object+" << sizeof(Osmium::OSM::Node)                     - sizeof(Osmium::OSM::Object) << ")\n";
+    std::cout << "sizeof(Osmium::OSM::Way)="                      << sizeof(Osmium::OSM::Way)                      << "  (Object+" << sizeof(Osmium::OSM::Way)                      - sizeof(Osmium::OSM::Object) << ")\n";
+    std::cout << "sizeof(Osmium::OSM::Relation)="                 << sizeof(Osmium::OSM::Relation)                 << "  (Object+" << sizeof(Osmium::OSM::Relation)                 - sizeof(Osmium::OSM::Object) << ")\n";
+    std::cout << "sizeof(Osmium::OSM::Multipolygon)="             << sizeof(Osmium::OSM::Multipolygon)             << "  (Object+" << sizeof(Osmium::OSM::Multipolygon)             - sizeof(Osmium::OSM::Object) << ")\n";
+    std::cout << "sizeof(Osmium::OSM::MultipolygonFromWay)="      << sizeof(Osmium::OSM::MultipolygonFromWay)      << "  (Object+" << sizeof(Osmium::OSM::MultipolygonFromWay)      - sizeof(Osmium::OSM::Object) << ")\n";
+    std::cout << "sizeof(Osmium::OSM::MultipolygonFromRelation)=" << sizeof(Osmium::OSM::MultipolygonFromRelation) << "  (Object+" << sizeof(Osmium::OSM::MultipolygonFromRelation) - sizeof(Osmium::OSM::Object) << ")\n";
+    std::cout << "sizeof(Osmium::OSM::WayInfo)="                  << sizeof(Osmium::OSM::WayInfo) << "\n";
+    std::cout << "sizeof(Osmium::OSM::RingInfo)="                 << sizeof(Osmium::OSM::RingInfo) << "\n";
+}
+
