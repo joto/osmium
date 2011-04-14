01c00b404d54b5c96ebe497b59fd0eb042ab57c4
Jochen Topf <jochen@topf.org>
2011-04-14 16:42:18 +0200

    fixed test

--- test/broken_way_geometries/test.sh
+++ test/broken_way_geometries/test.sh
@@ -5,5 +5,5 @@
 
 rm -f tmp/way.*
 mkdir -p tmp
-../osmjs/osmjs --debug --include=../osmjs/js/osm2shape.js --javascript=broken_way_geometries/broken_way_geometries.js broken_way_geometries/broken_way_geometries.osm
+../osmjs/osmjs --debug --location-store=sparsetable --include=../osmjs/js/osm2shape.js --javascript=broken_way_geometries/broken_way_geometries.js broken_way_geometries/broken_way_geometries.osm
 
