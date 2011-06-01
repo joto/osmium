0e2ed08d7a1ca959865d6149fc4e4049ede19c0e
Jochen Topf <jochen@topf.org>
2011-06-01 17:17:52 +0200

    typo: Licanse -> License

--- examples/osmium_sizeof.cpp
+++ examples/osmium_sizeof.cpp
@@ -18,7 +18,7 @@ version 3 of the Licenses, or (at your option) any later version.
 
 Osmium is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE. See the GNU Lesser General Public Licanse and the GNU
+PARTICULAR PURPOSE. See the GNU Lesser General Public License and the GNU
 General Public License for more details.
 
 You should have received a copy of the Licenses along with Osmium. If not, see
