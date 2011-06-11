57adfc8786362f3ff186943ea264e2b5685403b2
Jochen Topf <jochen@topf.org>
2011-06-11 12:06:40 +0200

    add errno.h include

--- include/osmium/osmfile.hpp
+++ include/osmium/osmfile.hpp
@@ -26,6 +26,7 @@ You should have received a copy of the Licenses along with Osmium. If not, see
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <errno.h>
 
 namespace Osmium {
 
