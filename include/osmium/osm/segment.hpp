15d631ebac84589c20f5cd47b5a0cf1afb623817
Jochen Topf <jochen@topf.org>
2012-05-01 17:59:14 +0200

    Add Osmium::OSM::Segment/UndirectedSegment classes

---
+++ include/osmium/osm/segment.hpp
@@ -0,0 +1,75 @@
+#ifndef OSMIUM_OSM_SEGMENT_HPP
+#define OSMIUM_OSM_SEGMENT_HPP
+
+/*
+
+Copyright 2012 Jochen Topf <jochen@topf.org> and others (see README).
+
+This file is part of Osmium (https://github.com/joto/osmium).
+
+Osmium is free software: you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License or (at your option) the GNU
+General Public License as published by the Free Software Foundation, either
+version 3 of the Licenses, or (at your option) any later version.
+
+Osmium is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Lesser General Public License and the GNU
+General Public License for more details.
+
+You should have received a copy of the Licenses along with Osmium. If not, see
+<http://www.gnu.org/licenses/>.
+
+*/
+
+#include <osmium/osm/position.hpp>
+
+namespace Osmium {
+
+    namespace OSM {
+
+        /**
+         * A Segment is the directed connection between two Positions.
+         */
+        class Segment : boost::equality_comparable<Segment> {
+
+        public:
+
+            Segment(const Osmium::OSM::Position& p1, const Osmium::OSM::Position& p2) :
+                m_first(p1),
+                m_second(p2) {
+            }
+
+            /// Return first Position of Segment.
+            const Osmium::OSM::Position first() const {
+                return m_first;
+            }
+
+            /// Return second Position of Segment.
+            const Osmium::OSM::Position second() const {
+                return m_second;
+            }
+
+        protected:
+
+            void swap_positions() {
+                std::swap(m_first, m_second);
+            }
+
+        private:
+
+            Osmium::OSM::Position m_first;
+            Osmium::OSM::Position m_second;
+
+        }; // class Segment
+
+        /// Segments are equal if both their positions are equal
+        bool operator==(const Segment& lhs, const Segment& rhs) {
+            return lhs.first() == rhs.first() && lhs.second() == rhs.second();
+        }
+
+    } // namespace OSM
+
+} // namespace Osmium
+
+#endif // OSMIUM_OSM_SEGMENT_HPP
---
+++ include/osmium/osm/undirected_segment.hpp
@@ -0,0 +1,66 @@
+#ifndef OSMIUM_OSM_UNDIRECTED_SEGMENT_HPP
+#define OSMIUM_OSM_UNDIRECTED_SEGMENT_HPP
+
+/*
+
+Copyright 2012 Jochen Topf <jochen@topf.org> and others (see README).
+
+This file is part of Osmium (https://github.com/joto/osmium).
+
+Osmium is free software: you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License or (at your option) the GNU
+General Public License as published by the Free Software Foundation, either
+version 3 of the Licenses, or (at your option) any later version.
+
+Osmium is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Lesser General Public License and the GNU
+General Public License for more details.
+
+You should have received a copy of the Licenses along with Osmium. If not, see
+<http://www.gnu.org/licenses/>.
+
+*/
+
+#include <osmium/osm/segment.hpp>
+
+namespace Osmium {
+
+    namespace OSM {
+
+        /**
+         * Undirected connection between two Positions. The first Position is
+         * always equal or "smaller" than the second Position, ie to the left
+         * and down.
+         */
+        class UndirectedSegment : boost::less_than_comparable<UndirectedSegment>, public Segment {
+
+        public:
+
+            UndirectedSegment(const Osmium::OSM::Position& p1, const Osmium::OSM::Position& p2) :
+                Segment(p1, p2) {
+                if (p2 < p1) {
+                    swap_positions();
+                }
+            }
+
+        };
+
+        /**
+        * UndirectedSegments are "smaller" if they are to the left and down of another
+        * segment. The first() position is checked first() and only if they have the
+        * same first() position the second() position is taken into account.
+        */
+        bool operator<(const UndirectedSegment& lhs, const UndirectedSegment& rhs) {
+            if (lhs.first() == rhs.first()) {
+                return lhs.second() < rhs.second();
+            } else {
+                return lhs.first() < rhs.first();
+            }
+        }
+
+    } // namespace OSM
+
+} // namespace Osmium
+
+#endif // OSMIUM_OSM_UNDIRECTED_SEGMENT_HPP
