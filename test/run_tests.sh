bab1cc32bc341991832346c11bd5ea830ffbbda7
Dmitry Marakasov <amdmi3@amdmi3.ru>
2013-05-23 00:29:06 +0400

    Allow to override CXX/CXXFLAGS through environment

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -10,8 +10,10 @@
 
 set -e
 
-CXX="g++"
-CXXFLAGS="-g -Wall -Wextra -Wredundant-decls -Wdisabled-optimization -pedantic -Wctor-dtor-privacy -Wnon-virtual-dtor -Woverloaded-virtual -Wsign-promo -Wno-long-long"
+if [ -z "$CXX" ]; then
+	CXX="g++"
+fi
+CXXFLAGS="$CXXFLAGS -g -Wall -Wextra -Wredundant-decls -Wdisabled-optimization -pedantic -Wctor-dtor-privacy -Wnon-virtual-dtor -Woverloaded-virtual -Wsign-promo -Wno-long-long"
 COMPILE="$CXX -I../include -I. $CXXFLAGS -o tests"
 
 if [ "x$1" = "x-v" ]; then
